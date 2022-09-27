59fac011a46f297f698de21551c4b4790d4b4c54
inventivetalent <mail@inventivetalent.org>
2022-09-27 15:31:35 +0200

    test multiple

--- src/test/java/org/mineskin/test/GenerateTest.java
+++ src/test/java/org/mineskin/test/GenerateTest.java
@@ -1,5 +1,6 @@
 package org.mineskin.test;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.mineskin.MineskinClient;
 import org.mineskin.SkinOptions;
@@ -17,60 +18,76 @@ import static org.junit.Assert.assertNotNull;
 
 public class GenerateTest {
 
-	private final MineskinClient client = new MineskinClient("MineskinJavaClient-Test");
+    private final MineskinClient client = new MineskinClient("MineskinJavaClient-Test");
 
-	@Test(timeout = 40000L)
-	public void urlTest() throws InterruptedException {
-		Thread.sleep(7000);
+    @Before
+    public void before() throws InterruptedException {
+        Thread.sleep(5000);
+    }
 
-		final String name = "JavaClient-Url";
-		Skin skin = this.client.generateUrl("https://i.imgur.com/jkhZKDX.png", SkinOptions.name(name)).join();
-		validateSkin(skin, name);
-		Thread.sleep(1000);
-	}
+    @Test(timeout = 40_000L)
+    public void urlTest() throws InterruptedException {
+        Thread.sleep(1000);
 
-	@Test(timeout = 40000L)
-	public void uploadTest() throws InterruptedException, IOException {
-		Thread.sleep(7000);
+        final String name = "JavaClient-Url";
+        System.out.println("#urlTest");
+        Skin skin = this.client.generateUrl("https://i.imgur.com/jkhZKDX.png", SkinOptions.name(name)).join();
+        validateSkin(skin, name);
+        Thread.sleep(1000);
+    }
 
-		final String name = "JavaClient-Upload";
-		File file = File.createTempFile("mineskin-temp-upload-image", ".png");
-		ImageIO.write(randomImage(64, 32), "png", file);
-		Skin skin = this.client.generateUpload(file, SkinOptions.name(name)).join();
-		validateSkin(skin, name);
-		Thread.sleep(1000);
-	}
+    @Test(timeout = 40_000L)
+    public void uploadTest() throws InterruptedException, IOException {
+        Thread.sleep(1000);
 
-	@Test(timeout = 40000L)
-	public void uploadRenderedImageTest() throws InterruptedException, IOException {
-		Thread.sleep(7000);
+        final String name = "JavaClient-Upload";
+        File file = File.createTempFile("mineskin-temp-upload-image", ".png");
+        ImageIO.write(randomImage(64, 32), "png", file);
+        System.out.println("#uploadTest");
+        Skin skin = this.client.generateUpload(file, SkinOptions.name(name)).join();
+        validateSkin(skin, name);
+        Thread.sleep(1000);
+    }
 
-		final String name = "JavaClient-Upload";
-		Skin skin = this.client.generateUpload(randomImage(64, 32), SkinOptions.name(name)).join();
-		validateSkin(skin, name);
-		Thread.sleep(1000);
-	}
+    @Test(timeout = 40_000L)
+    public void uploadRenderedImageTest() throws InterruptedException, IOException {
+        Thread.sleep(1000);
 
-	void validateSkin(Skin skin, String name) {
-		assertNotNull(skin);
-		assertNotNull(skin.data);
-		assertNotNull(skin.data.texture);
+        final String name = "JavaClient-Upload";
+        System.out.println("#uploadRenderedImageTest");
+        Skin skin = this.client.generateUpload(randomImage(64, 32), SkinOptions.name(name)).join();
+        validateSkin(skin, name);
+        Thread.sleep(1000);
+    }
 
-		assertEquals(name, skin.name);
-	}
+    @Test(timeout = 160_000L)
+    public void multiUploadTest() throws InterruptedException, IOException {
+        for (int i = 0; i < 2; i++) {
+            uploadTest();
+            uploadRenderedImageTest();
+        }
+    }
 
-	BufferedImage randomImage(int width, int height) {
-		Random random = new Random();
-		BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-		for (int x = 0; x < width; x++) {
-			for (int y = 0; y < height; y++) {
-				float r = random.nextFloat();
-				float g = random.nextFloat();
-				float b = random.nextFloat();
-				image.setRGB(x, y, new Color(r, g, b).getRGB());
-			}
-		}
-		return image;
-	}
+    void validateSkin(Skin skin, String name) {
+        assertNotNull(skin);
+        assertNotNull(skin.data);
+        assertNotNull(skin.data.texture);
+
+        assertEquals(name, skin.name);
+    }
+
+    BufferedImage randomImage(int width, int height) {
+        Random random = new Random();
+        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                float r = random.nextFloat();
+                float g = random.nextFloat();
+                float b = random.nextFloat();
+                image.setRGB(x, y, new Color(r, g, b).getRGB());
+            }
+        }
+        return image;
+    }
 
 }
